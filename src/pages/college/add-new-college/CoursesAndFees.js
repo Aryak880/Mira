ddd585f1803c74b10cfc887ac92dc18af0e7380d
Aryak880 <aryaksinghchauhan@gmail.com>
2023-11-17 13:24:44 +0530

    fixed issue of prettier/prettier

--- src/pages/college/add-new-college/CoursesAndFees.js
+++ src/pages/college/add-new-college/CoursesAndFees.js
@@ -1,8 +1,18 @@
-import React, {useState} from 'react'
-import { Container, Grid, TextField, Select, MenuItem, FormControl, InputLabel, Typography , Button} from '@mui/material'
+import React, { useState } from "react";
+import {
+  Container,
+  Grid,
+  TextField,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Typography,
+  Button,
+} from "@mui/material";
 
 const CoursesAndFees = () => {
-    const [anotherCourse, setAnotherCourse] = useState([])
+  const [anotherCourse, setAnotherCourse] = useState([]);
   return (
     <Container>
       <form>
@@ -20,7 +30,7 @@ const CoursesAndFees = () => {
             </FormControl>
           </Grid>
           <Grid item xs={4}>
-          <Typography>Course Duration</Typography>
+            <Typography>Course Duration</Typography>
             <FormControl fullWidth>
               <InputLabel>Select/ Type Here</InputLabel>
               <Select>
@@ -31,7 +41,7 @@ const CoursesAndFees = () => {
             </FormControl>
           </Grid>
           <Grid item xs={4}>
-          <Typography>Degree Offered</Typography>
+            <Typography>Degree Offered</Typography>
             <FormControl fullWidth>
               <InputLabel>Select/ Type Here</InputLabel>
               <Select>
@@ -55,159 +65,162 @@ const CoursesAndFees = () => {
 
           <Grid item xs={4}>
             <Grid item>
-                <Typography>Total Fees</Typography>
-                <FormControl fullWidth>
+              <Typography>Total Fees</Typography>
+              <FormControl fullWidth>
                 <InputLabel>Select/ Type Here</InputLabel>
                 <Select>
-                    <MenuItem value="fees1">Fees 1</MenuItem>
-                    <MenuItem value="fees2">Fees 2</MenuItem>
-                    {/* Add more fees options as needed */}
+                  <MenuItem value="fees1">Fees 1</MenuItem>
+                  <MenuItem value="fees2">Fees 2</MenuItem>
+                  {/* Add more fees options as needed */}
                 </Select>
-                </FormControl>
+              </FormControl>
             </Grid>
             <Grid item>
-                <Typography>Eligibility Criteria</Typography>
-                <FormControl fullWidth>
+              <Typography>Eligibility Criteria</Typography>
+              <FormControl fullWidth>
                 <InputLabel>Select/ Type Here</InputLabel>
                 <Select>
-                    <MenuItem value="criteria1">Criteria 1</MenuItem>
-                    <MenuItem value="criteria2">Criteria 2</MenuItem>
-                    {/* Add more eligibility criteria options as needed */}
+                  <MenuItem value="criteria1">Criteria 1</MenuItem>
+                  <MenuItem value="criteria2">Criteria 2</MenuItem>
+                  {/* Add more eligibility criteria options as needed */}
                 </Select>
-                </FormControl>
+              </FormControl>
             </Grid>
           </Grid>
 
           <Grid item xs={4}>
             <Grid item>
-                <Typography>Total Fees</Typography>
-                <FormControl fullWidth>
+              <Typography>Total Fees</Typography>
+              <FormControl fullWidth>
                 <InputLabel>Select/ Type Here</InputLabel>
                 <Select>
-                    <MenuItem value="fees1">Fees 1</MenuItem>
-                    <MenuItem value="fees2">Fees 2</MenuItem>
-                    {/* Add more fees options as needed */}
+                  <MenuItem value="fees1">Fees 1</MenuItem>
+                  <MenuItem value="fees2">Fees 2</MenuItem>
+                  {/* Add more fees options as needed */}
                 </Select>
-                </FormControl>
+              </FormControl>
             </Grid>
             <Grid item>
-                <Typography>Eligibility Criteria</Typography>
-                <FormControl fullWidth>
+              <Typography>Eligibility Criteria</Typography>
+              <FormControl fullWidth>
                 <InputLabel>Select/ Type Here</InputLabel>
                 <Select>
-                    <MenuItem value="criteria1">Criteria 1</MenuItem>
-                    <MenuItem value="criteria2">Criteria 2</MenuItem>
-                    {/* Add more eligibility criteria options as needed */}
+                  <MenuItem value="criteria1">Criteria 1</MenuItem>
+                  <MenuItem value="criteria2">Criteria 2</MenuItem>
+                  {/* Add more eligibility criteria options as needed */}
                 </Select>
-                </FormControl>
+              </FormControl>
             </Grid>
           </Grid>
         </Grid>
 
-        {
-            anotherCourse.map((data, index) => (
-                <Grid container spacing={3} key={index}>
-                    {/* First Row */}
-                    <Grid item xs={4}>
-                        <Typography>Select Exam for the course</Typography>
-                        <FormControl fullWidth>
-                        <InputLabel>Select/ Type Here</InputLabel>
-                        <Select>
-                            <MenuItem value="exam1">Exam 1</MenuItem>
-                            <MenuItem value="exam2">Exam 2</MenuItem>
-                            {/* Add more exam options as needed */}
-                        </Select>
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={4}>
-                    <Typography>Course Duration</Typography>
-                        <FormControl fullWidth>
-                        <InputLabel>Select/ Type Here</InputLabel>
-                        <Select>
-                            <MenuItem value="1">1 Year</MenuItem>
-                            <MenuItem value="2">2 Years</MenuItem>
-                            {/* Add more duration options as needed */}
-                        </Select>
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={4}>
-                    <Typography>Degree Offered</Typography>
-                        <FormControl fullWidth>
-                        <InputLabel>Select/ Type Here</InputLabel>
-                        <Select>
-                            <MenuItem value="degree1">Degree 1</MenuItem>
-                            <MenuItem value="degree2">Degree 2</MenuItem>
-                            {/* Add more degree options as needed */}
-                        </Select>
-                        </FormControl>
-                    </Grid>
+        {anotherCourse.map((data, index) => (
+          <Grid container spacing={3} key={index}>
+            {/* First Row */}
+            <Grid item xs={4}>
+              <Typography>Select Exam for the course</Typography>
+              <FormControl fullWidth>
+                <InputLabel>Select/ Type Here</InputLabel>
+                <Select>
+                  <MenuItem value="exam1">Exam 1</MenuItem>
+                  <MenuItem value="exam2">Exam 2</MenuItem>
+                  {/* Add more exam options as needed */}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography>Course Duration</Typography>
+              <FormControl fullWidth>
+                <InputLabel>Select/ Type Here</InputLabel>
+                <Select>
+                  <MenuItem value="1">1 Year</MenuItem>
+                  <MenuItem value="2">2 Years</MenuItem>
+                  {/* Add more duration options as needed */}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography>Degree Offered</Typography>
+              <FormControl fullWidth>
+                <InputLabel>Select/ Type Here</InputLabel>
+                <Select>
+                  <MenuItem value="degree1">Degree 1</MenuItem>
+                  <MenuItem value="degree2">Degree 2</MenuItem>
+                  {/* Add more degree options as needed */}
+                </Select>
+              </FormControl>
+            </Grid>
 
-                    {/* Second Row */}
-                    <Grid item xs={4}>
-                        <Typography>Course Description</Typography>
-                        <TextField
-                        fullWidth
-                        label="Course Description"
-                        multiline
-                        rows={2}
-                        />
-                    </Grid>
+            {/* Second Row */}
+            <Grid item xs={4}>
+              <Typography>Course Description</Typography>
+              <TextField
+                fullWidth
+                label="Course Description"
+                multiline
+                rows={2}
+              />
+            </Grid>
 
-                    <Grid item xs={4}>
-                        <Grid item>
-                            <Typography>Total Fees</Typography>
-                            <FormControl fullWidth>
-                            <InputLabel>Select/ Type Here</InputLabel>
-                            <Select>
-                                <MenuItem value="fees1">Fees 1</MenuItem>
-                                <MenuItem value="fees2">Fees 2</MenuItem>
-                                {/* Add more fees options as needed */}
-                            </Select>
-                            </FormControl>
-                        </Grid>
-                        <Grid item>
-                            <Typography>Eligibility Criteria</Typography>
-                            <FormControl fullWidth>
-                            <InputLabel>Select/ Type Here</InputLabel>
-                            <Select>
-                                <MenuItem value="criteria1">Criteria 1</MenuItem>
-                                <MenuItem value="criteria2">Criteria 2</MenuItem>
-                                {/* Add more eligibility criteria options as needed */}
-                            </Select>
-                            </FormControl>
-                        </Grid>
-                    </Grid>
+            <Grid item xs={4}>
+              <Grid item>
+                <Typography>Total Fees</Typography>
+                <FormControl fullWidth>
+                  <InputLabel>Select/ Type Here</InputLabel>
+                  <Select>
+                    <MenuItem value="fees1">Fees 1</MenuItem>
+                    <MenuItem value="fees2">Fees 2</MenuItem>
+                    {/* Add more fees options as needed */}
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item>
+                <Typography>Eligibility Criteria</Typography>
+                <FormControl fullWidth>
+                  <InputLabel>Select/ Type Here</InputLabel>
+                  <Select>
+                    <MenuItem value="criteria1">Criteria 1</MenuItem>
+                    <MenuItem value="criteria2">Criteria 2</MenuItem>
+                    {/* Add more eligibility criteria options as needed */}
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
 
-                    <Grid item xs={4}>
-                        <Grid item>
-                            <Typography>Total Fees</Typography>
-                            <FormControl fullWidth>
-                            <InputLabel>Select/ Type Here</InputLabel>
-                            <Select>
-                                <MenuItem value="fees1">Fees 1</MenuItem>
-                                <MenuItem value="fees2">Fees 2</MenuItem>
-                                {/* Add more fees options as needed */}
-                            </Select>
-                            </FormControl>
-                        </Grid>
-                        <Grid item>
-                            <Typography>Eligibility Criteria</Typography>
-                            <FormControl fullWidth>
-                            <InputLabel>Select/ Type Here</InputLabel>
-                            <Select>
-                                <MenuItem value="criteria1">Criteria 1</MenuItem>
-                                <MenuItem value="criteria2">Criteria 2</MenuItem>
-                                {/* Add more eligibility criteria options as needed */}
-                            </Select>
-                            </FormControl>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            ))
-        }
+            <Grid item xs={4}>
+              <Grid item>
+                <Typography>Total Fees</Typography>
+                <FormControl fullWidth>
+                  <InputLabel>Select/ Type Here</InputLabel>
+                  <Select>
+                    <MenuItem value="fees1">Fees 1</MenuItem>
+                    <MenuItem value="fees2">Fees 2</MenuItem>
+                    {/* Add more fees options as needed */}
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item>
+                <Typography>Eligibility Criteria</Typography>
+                <FormControl fullWidth>
+                  <InputLabel>Select/ Type Here</InputLabel>
+                  <Select>
+                    <MenuItem value="criteria1">Criteria 1</MenuItem>
+                    <MenuItem value="criteria2">Criteria 2</MenuItem>
+                    {/* Add more eligibility criteria options as needed */}
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+          </Grid>
+        ))}
 
-        <Button m={2} variant="contained" color="primary" onClick={() => setAnotherCourse([...anotherCourse, ""])}>
-           + Add More Courses
+        <Button
+          m={2}
+          variant="contained"
+          color="primary"
+          onClick={() => setAnotherCourse([...anotherCourse, ""])}
+        >
+          + Add More Courses
         </Button>
       </form>
     </Container>
--- src/pages/college/add-new-college/EntranceExams.js
+++ src/pages/college/add-new-college/EntranceExams.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { TextField, Button, Grid, Typography, Checkbox } from "@mui/material";
+import { TextField, Button, Grid, Typography } from "@mui/material";
 
 const EntranceExams = () => {
   const [checkboxes, setCheckboxes] = useState({
@@ -33,29 +33,26 @@ const EntranceExams = () => {
 
   // Function to handle checkbox changes
   const handleCheckboxChange = (field, index) => {
-
-    if(field === 0){
-      const design = checkboxes.design
-      design[index].selected = !design[index].selected
-      setCheckboxes({...checkboxes, design})
+    if (field === 0) {
+      const design = checkboxes.design;
+      design[index].selected = !design[index].selected;
+      setCheckboxes({ ...checkboxes, design });
     }
-    if(field === 1){
-      const engineering = checkboxes.engineering
-      engineering[index].selected = !engineering[index].selected
-      setCheckboxes({...checkboxes, engineering})
+    if (field === 1) {
+      const engineering = checkboxes.engineering;
+      engineering[index].selected = !engineering[index].selected;
+      setCheckboxes({ ...checkboxes, engineering });
     }
-    if(field === 2){
-      const medical = checkboxes.medical
-      medical[index].selected = !medical[index].selected
-      setCheckboxes({...checkboxes, medical})
+    if (field === 2) {
+      const medical = checkboxes.medical;
+      medical[index].selected = !medical[index].selected;
+      setCheckboxes({ ...checkboxes, medical });
     }
-    if(field === 3){
-      const law = checkboxes.law
-      law[index].selected = !law[index].selected
-      setCheckboxes({...checkboxes, law})
+    if (field === 3) {
+      const law = checkboxes.law;
+      law[index].selected = !law[index].selected;
+      setCheckboxes({ ...checkboxes, law });
     }
-    
-    
   };
 
   // Function to handle text input change
@@ -65,134 +62,137 @@ const EntranceExams = () => {
 
   return (
     <>
-    <Typography variant="h5" mb={2}>ENTRANCE EXAMS</Typography>
-    <Typography variant="p">Select all the exams through which students can get 
-    admission in your college/ institute</Typography>
-    <form>
-      {/* First Row */}
-      <Grid container mb={2} mt={2}>
-      <Grid  item xs={4}>
-        <Typography>Design</Typography>
-        <Grid style={{display: 'flex', flexDirection: 'end'}}>
-        {checkboxes.design.map(({ exam, selected }, index) => (
-          <Grid  key={index} mr={1}>
-            <Button
-              variant={selected ? "contained" : "outlined"}
-              color="primary"
-              onClick={() => handleCheckboxChange(0, index)}
-            >
-              {`${exam}`}
-            </Button>
-          </Grid>
-        ))}
-        </Grid>
-      </Grid>
-
-      <Grid  item xs={4}>
-        <Typography>Engineering</Typography>
-        <Grid style={{display: 'flex'}}>
-        {checkboxes.engineering.map(({ exam, selected }, index) => (
-          <Grid  key={index} mr={1}>
-            <Button
-              variant={selected ? "contained" : "outlined"}
-              color="primary"
-              onClick={() => handleCheckboxChange(1, index)}
-            >
-              {`${exam}`}
-            </Button>
-          </Grid>
-        ))}
-        </Grid>
-      </Grid>
-
-      <Grid  item xs={4}>
-        <Typography>Medical</Typography>
-        <Grid style={{display: 'flex'}}>
-        {checkboxes.medical.map(({ exam, selected }, index) => (
-          <Grid  key={index} mr={1}>
-            <Button
-              variant={selected ? "contained" : "outlined"}
-              color="primary"
-              onClick={() => handleCheckboxChange(2, index)}
-            >
-              {`${exam}`}
-            </Button>
-          </Grid>
-        ))}
-        </Grid>
-      </Grid>
-
-      </Grid>
-
-      {/* Second Row */}
-      <Grid container mb={2}>
-        <Grid  item xs={4}>
-          <Typography>Law</Typography>
-          <Grid style={{display: 'flex'}}>
-          {checkboxes.law.map(({ exam, selected }, index) => (
-            <Grid  key={index} mr={1}>
-              <Button
-                variant={selected ? "contained" : "outlined"}
-                color="primary"
-                onClick={() => handleCheckboxChange(3, index)}
-              >
-                {`${exam}`}
-              </Button>
+      <Typography variant="h5" mb={2}>
+        ENTRANCE EXAMS
+      </Typography>
+      <Typography variant="p">
+        Select all the exams through which students can get admission in your
+        college/ institute
+      </Typography>
+      <form>
+        {/* First Row */}
+        <Grid container mb={2} mt={2}>
+          <Grid item xs={4}>
+            <Typography>Design</Typography>
+            <Grid style={{ display: "flex" }}>
+              {checkboxes.design.map(({ exam, selected }, index) => (
+                <Grid key={index} mr={1}>
+                  <Button
+                    variant={selected ? "contained" : "outlined"}
+                    color="primary"
+                    onClick={() => handleCheckboxChange(0, index)}
+                  >
+                    {`${exam}`}
+                  </Button>
+                </Grid>
+              ))}
             </Grid>
-          ))}
           </Grid>
-        </Grid>
 
-        <Grid  item xs={4}>
-          <Typography>Engineering</Typography>
-          <Grid style={{display: 'flex'}}>
-          {checkboxes.engineering.map(({ exam, selected }, index) => (
-            <Grid  key={index} mr={1}>
-              <Button
-                variant={selected ? "contained" : "outlined"}
-                color="primary"
-                onClick={() => handleCheckboxChange(1, index)}
-              >
-                {`${exam}`}
-              </Button>
+          <Grid item xs={4}>
+            <Typography>Engineering</Typography>
+            <Grid style={{ display: "flex" }}>
+              {checkboxes.engineering.map(({ exam, selected }, index) => (
+                <Grid key={index} mr={1}>
+                  <Button
+                    variant={selected ? "contained" : "outlined"}
+                    color="primary"
+                    onClick={() => handleCheckboxChange(1, index)}
+                  >
+                    {`${exam}`}
+                  </Button>
+                </Grid>
+              ))}
             </Grid>
-          ))}
           </Grid>
-        </Grid>
 
-        <Grid  item xs={4}>
-          <Typography>Medical</Typography>
-          <Grid style={{display: 'flex'}}>
-          {checkboxes.medical.map(({ exam, selected }, index) => (
-            <Grid  key={index} mr={1}>
-              <Button
-                variant={selected ? "contained" : "outlined"}
-                color="primary"
-                onClick={() => handleCheckboxChange(2, index)}
-              >
-                {`${exam}`}
-              </Button>
+          <Grid item xs={4}>
+            <Typography>Medical</Typography>
+            <Grid style={{ display: "flex" }}>
+              {checkboxes.medical.map(({ exam, selected }, index) => (
+                <Grid key={index} mr={1}>
+                  <Button
+                    variant={selected ? "contained" : "outlined"}
+                    color="primary"
+                    onClick={() => handleCheckboxChange(2, index)}
+                  >
+                    {`${exam}`}
+                  </Button>
+                </Grid>
+              ))}
             </Grid>
-          ))}
           </Grid>
         </Grid>
 
-      </Grid>
+        {/* Second Row */}
+        <Grid container mb={2}>
+          <Grid item xs={4}>
+            <Typography>Law</Typography>
+            <Grid style={{ display: "flex" }}>
+              {checkboxes.law.map(({ exam, selected }, index) => (
+                <Grid key={index} mr={1}>
+                  <Button
+                    variant={selected ? "contained" : "outlined"}
+                    color="primary"
+                    onClick={() => handleCheckboxChange(3, index)}
+                  >
+                    {`${exam}`}
+                  </Button>
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
 
+          <Grid item xs={4}>
+            <Typography>Engineering</Typography>
+            <Grid style={{ display: "flex" }}>
+              {checkboxes.engineering.map(({ exam, selected }, index) => (
+                <Grid key={index} mr={1}>
+                  <Button
+                    variant={selected ? "contained" : "outlined"}
+                    color="primary"
+                    onClick={() => handleCheckboxChange(1, index)}
+                  >
+                    {`${exam}`}
+                  </Button>
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
 
-      {/* Third Row */}
-      <Grid container mb={2}>
-        <Grid item xs={4}>
-          <Typography>Other (If any entrance/criteria is not in the list)</Typography>
-          <TextField
-            placeholder="Type Here"
-            fullWidth
-            value={textInput}
-            onChange={handleTextInputChange}
-          />
+          <Grid item xs={4}>
+            <Typography>Medical</Typography>
+            <Grid style={{ display: "flex" }}>
+              {checkboxes.medical.map(({ exam, selected }, index) => (
+                <Grid key={index} mr={1}>
+                  <Button
+                    variant={selected ? "contained" : "outlined"}
+                    color="primary"
+                    onClick={() => handleCheckboxChange(2, index)}
+                  >
+                    {`${exam}`}
+                  </Button>
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
         </Grid>
-      </Grid>
-    </form>
+
+        {/* Third Row */}
+        <Grid container mb={2}>
+          <Grid item xs={4}>
+            <Typography>
+              Other (If any entrance/criteria is not in the list)
+            </Typography>
+            <TextField
+              placeholder="Type Here"
+              fullWidth
+              value={textInput}
+              onChange={handleTextInputChange}
+            />
+          </Grid>
+        </Grid>
+      </form>
     </>
   );
 };
